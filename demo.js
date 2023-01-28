849c2ee5ab18e1b046e4ac906b640c6df45830da
petaharitha <peta.haritha26@gmail.com>
2023-01-28 12:56:30 +0530

    fifth commit

--- demo.js
+++ demo.js
@@ -5,12 +5,19 @@ console.log(headerTitle);
 //headerTitle.innerHTML='<h3>hello<h3>';
 //headerTitle.style.borderBottom='solid 3px #000';
 //headerTitle.style.border='solid 3px #000';
-var li=document.getElementsByTagName('li');
-console.log(li);
-console.log(li[1]);
-li[1].style.fontWeight='bold';
-li[2].style.backgroundColor='green';
-for(var i=0;i<li.length;i++)
+//var li=document.getElementsByTagName('li');
+//console.log(li);
+//console.log(li[1]);
+//li[1].style.fontWeight='bold';
+//li[2].style.backgroundColor='green';
+//for(var i=0;i<li.length;i++)
+//{
+ //   li[i].style.fontWeight='bold';
+//}
+var secondItem=document.querySelector('.list-group-item:nth-child(2)');
+secondItem.style.color='green';
+var odd=document.querySelectorAll('li:nth-child(odd');
+for(var i=0;i<odd.length;i++)
 {
-    li[i].style.fontWeight='bold';
+    odd[i].style.backgroundColor='green';
 }
