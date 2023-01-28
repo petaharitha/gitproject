84b11aee2e15c4572bbca637ae74ff3407dcbd68
petaharitha <peta.haritha26@gmail.com>
2023-01-28 12:27:15 +0530

    forth commit

--- demo.js
+++ demo.js
@@ -5,13 +5,12 @@ console.log(headerTitle);
 //headerTitle.innerHTML='<h3>hello<h3>';
 //headerTitle.style.borderBottom='solid 3px #000';
 //headerTitle.style.border='solid 3px #000';
-var items=document.getElementsByClassName('list-group-item');
-console.log(items);
-console.log(items[1]);
-items[1].textContent='hello 2';
-//items[1].style.fontWeight='bold';
-items[2].style.backgroundColor='green';
-for(var i=0;i<items.length;i++)
+var li=document.getElementsByTagName('li');
+console.log(li);
+console.log(li[1]);
+li[1].style.fontWeight='bold';
+li[2].style.backgroundColor='green';
+for(var i=0;i<li.length;i++)
 {
-    items[i].style.fontWeight='bold';
+    li[i].style.fontWeight='bold';
 }
