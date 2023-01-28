362a2a9ac845e3466499087e760d4090695c252d
petaharitha <peta.haritha26@gmail.com>
2023-01-28 12:09:03 +0530

    third commit on demo

--- demo.js
+++ demo.js
@@ -2,13 +2,16 @@ var headerTitle=document.getElementById('header-title');
 console.log(headerTitle);
 //headerTitle.textContent="Hello";
 //headerTitle.innerText="goodbye";
-headerTitle.innerHTML='<h3>hello<h3>';
-//headerTitle.style.borderBottom='solid 3px blue';
-headerTitle.style.border='solid black';
-var title=document.getElementsByClassName('title');
-console.log(title);
-console.log(title[0]);
-for(var i=0;i<title.length;i++)
+//headerTitle.innerHTML='<h3>hello<h3>';
+//headerTitle.style.borderBottom='solid 3px #000';
+//headerTitle.style.border='solid 3px #000';
+var items=document.getElementsByClassName('list-group-item');
+console.log(items);
+console.log(items[1]);
+items[1].textContent='hello 2';
+//items[1].style.fontWeight='bold';
+items[2].style.backgroundColor='green';
+for(var i=0;i<items.length;i++)
 {
-    title[0].style.backgroundcolor='yellow';
+    items[i].style.fontWeight='bold';
 }
