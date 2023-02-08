08dbdc01a0e019fb6912bc7692340e76a2d316ee
petaharitha <peta.haritha26@gmail.com>
2023-02-08 15:48:53 +0530

    appendlist commit

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
+function test(){
 let myobj={
-    name:"haritha",
-    age:26
+    name:document.getElementById("username").value,
+    email:document.getElementById("email").value,
+    password:document.getElementById("password").value,
 };
 let myobj_serialized=JSON.stringify(myobj);
 console.log(myobj_serialized);
@@ -9,3 +11,10 @@ console.log(localStorage);
 
 let myobj_deserialized=JSON.parse(localStorage.getItem(myobj));
 console.log(myobj_deserialized);
+showUseronscreen(myobj_deserialized);
+showUseronscreen(myobj_deserialized)
+{
+    var parentEle=document.getelementById("list of Items");
+    parentEle.innerHTML=<li>$(myobj_deserialized.username) - $(myobj_deserialized.email) - $(myobj_deserialized.password)</li>
+}
+}
